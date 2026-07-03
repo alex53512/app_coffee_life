20942180cbccd4269cb452d2c647850ca5b7164b
Valentina <valentina@correo.com>
2026-07-03 10:44:27 -0500

    wip: workaround +1 dia en fecha mientras Mariana corrige timezone

--- lib/screens/diagnostic_screen.dart
+++ lib/screens/diagnostic_screen.dart
@@ -1050,8 +1050,8 @@ class _DiagnosticScreenState extends State<DiagnosticScreen> {
 
         int? idMonitoreo;
         try {
-          final hoy = DateTime.now();
-          final fechaStr = '${hoy.year}-${hoy.month.toString().padLeft(2, '0')}-${hoy.day.toString().padLeft(2, '0')}T00:00:00.000-05:00';
+          final manana = DateTime.now().add(const Duration(days: 1));
+          final fechaStr = '${manana.year}-${manana.month.toString().padLeft(2, '0')}-${manana.day.toString().padLeft(2, '0')}T00:00:00.000-05:00';
           final resMonitoreo = await ApiService.post('/monitoreos', {
             'id_cultivo':      _cultivoSeleccionado,
             'fecha_monitoreo': fechaStr,
